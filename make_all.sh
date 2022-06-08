3138d34208c0bc612a7991e758edd09d1447b056
David Castells-Rufas <david.castells@uab.cat>
2022-06-08 12:20:24 +0200

    kernel headers dir

--- make_all.sh
+++ make_all.sh
@@ -1,7 +1,7 @@
 # Need to use the exact version of gcc that was used to compile the kernel
 # It's in /usr/bin, so put it at the front of the path
 export PATH=/usr/bin:$PATH
-SRC_PATH=/usr/src/linux-headers-`uname -r`-`uname -m`
+SRC_PATH=/usr/src/kernels/`uname -r`-`uname -m`
 if [ ! -d "$SRC_PATH" ]
 then
 	SRC_PATH=/usr/src/linux-headers-`uname -r`
